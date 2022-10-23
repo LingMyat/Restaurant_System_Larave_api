6c2cede3f770134eb6dc96b2b5bfdce23cce9fbe
LingMyat <bizkits223@gmail.com>
2022-10-23 19:41:26 +0630

    all UI feature can use with api now

--- routes/api.php
+++ routes/api.php
@@ -24,27 +24,27 @@ Route::controller(ApiController::class)
     // Dish api
     Route::get('dishes','dishes');
     Route::get('dishes/{id}','showDish');
-    Route::post('dishes/create','createDish');
-    Route::post('dishes/update/{id}','updateDish');
+    Route::post('dishes/create','createDish');//you need to fill name/category_id/price
+    Route::post('dishes/update/{id}','updateDish');//you need to fill name/category_id/price
     Route::get('dishes/delete/{id}','deleteDish');
 
     // User api
     Route::get('users','users');
     Route::get('users/{id}','showUser');
-    Route::post('users/register','createUser');
+    Route::post('users/register','createUser');//you need to fill name/email/password
     Route::get('users/delete/{id}','deleteUser');
 
     // Category api
     Route::get('categories','categories');
     Route::get('categories/{id}','showCategory');
-    Route::post('categories/create','createCategory');
-    Route::post('categories/update/{id}','updateCategory');
+    Route::post('categories/create','createCategory');//you need to fill name
+    Route::post('categories/update/{id}','updateCategory');//you need to fill name
     Route::get('categories/delete/{id}','deleteCategory');
 
     //Table api
     Route::get('tables','tables');
     Route::get('tables/avaliable','avaliableTable');
-    Route::post('tables/add','addTables');//you need to faill quantity of tables that you want to add
+    Route::post('tables/add','addTables');//you need to fill quantity of tables that you want to add
 
     //Order api
     Route::get('orders','orders');
@@ -62,4 +62,5 @@ Route::controller(ApiController::class)
     // after making dishes order is ready(you can call order ready api)
     // then waiter can serve order to the dealing table(you can call order serve api)
     // after all done the customer billing the order(you can call order billing api)
+    // ready/serve/billing all method need to fill order_id
 });
