6b73006125c3109439a933e340f5f4c9c4975190
LingMyat <bizkits223@gmail.com>
2022-11-16 20:12:19 +0630

    login register ui switch

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -59,14 +59,7 @@
         </div>
       </form>
 
-      <div class="social-auth-links text-center mt-2 mb-3">
-        <a href="#" class="btn btn-block btn-primary">
-          <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-        </a>
-        <a href="#" class="btn btn-block btn-danger">
-          <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-        </a>
-      </div>
+
       <!-- /.social-auth-links -->
       <p class="mb-0">
         <a href="{{ route('auth#register') }}" class="text-center">Register a new membership</a>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -74,16 +74,6 @@
         </div>
       </form>
 
-      <div class="social-auth-links text-center">
-        <a href="#" class="btn btn-block btn-primary">
-          <i class="fab fa-facebook mr-2"></i>
-          Sign up using Facebook
-        </a>
-        <a href="#" class="btn btn-block btn-danger">
-          <i class="fab fa-google-plus mr-2"></i>
-          Sign up using Google+
-        </a>
-      </div>
 
       <a href="{{ route('auth#login') }}" class="text-center">I already have a membership</a>
     </div>
